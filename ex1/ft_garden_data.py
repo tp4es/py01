2f4cc33511720b4b5b428306b812e07a29d75db4
Tiago_laptop <tide-oli@student.42madrid.com>
2026-04-21 17:55:54 +0200

    flake8

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,33 +1,24 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_garden_data.py                                  :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: tide.oli <marvin@42.fr>                    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/04/14 14:23:32 by tide.oli          #+#    #+#              #
-#    Updated: 2026/04/14 18:30:32 by tide.oli         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!/usr/bin/env python3
 
-class	Plants():
-	def	__init__(self, name :str, height :int, age :int):
-		self.name = name
-		self.height = height
-		self.age = age
-	def	show(self):
-		print(f"{self.name.capitalize()}: {str(self.height)}cm, {str(self.age)} days old.")
-		
+class Plants():
+    def __init__(self, name: str, height: int, age: int):
+        self.name = name
+        self.height = height
+        self.age = age
 
-def	ft_garden_data():
-	plant1 = Plants("rose", 25, 30)
-	plant2 = Plants("sunflower", 80, 45)
-	plant3 = Plants("cactus", 15, 120)
-	plants_list = [plant1, plant2, plant3]
-	for plant_name in plants_list:
-		plant_name.show()
+    def show(self):
+        print(
+            f"{self.name.capitalize()}: {str(self.height)}cm, {str(self.age)} days old.")
+
+
+def ft_garden_data():
+    plant1 = Plants("rose", 25, 30)
+    plant2 = Plants("sunflower", 80, 45)
+    plant3 = Plants("cactus", 15, 120)
+    plants_list = [plant1, plant2, plant3]
+    for plant_name in plants_list:
+        plant_name.show()
+
 
 if __name__ == "__main__":
-	ft_garden_data()
+    ft_garden_data()
--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -1,171 +1,212 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_garden_analytics.py                             :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: tide.oli <marvin@42.fr>                    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/04/15 20:03:00 by tide.oli          #+#    #+#              #
-#    Updated: 2026/04/15 23:27:00 by tide.oli         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!/usr/bin/env python3
 
-class	Plants:
+class Plants:
 
-	count_show:int = 0
-	count_grow:int = 0
-	count_aged:int = 0
-	count_special:int = 0
-	def	__init__(self, name:str = "Unknown plant", heigh:int = 0, age:int = 0):
-		self.name = name
-		self._height = heigh
-		self._age = age
+    count_show: int = 0
+    count_grow: int = 0
+    count_aged: int = 0
+    count_special: int = 0
 
-	@classmethod
-	def	creat(cls):
-		name = "unknown plant"
-		age = 0
-		height = 0
-		return cls(name, height, age)
-	def	statistics(self):
-		print(f"\tTotal show calls: {self.count_show}")
-		print(f"\tTotal grow calls: {self.count_grow}")
-		print(f"\tTotal aged calls: {self.count_aged}")
-		print(f"\tTotal special calls: {self.count_special}")
+    def __init__(
+            self,
+            name: str = "Unknown plant",
+            heigh: int = 0,
+            age: int = 0):
+        self.name = name
+        self._height = heigh
+        self._age = age
 
-		""" === Getters and Setters for height and age with validation === """
-	@property
-	def proper_height(self):
-		return self._height
-	@proper_height.setter
-	def	proper_height(self, value: int):
-		if value > 0:
-			self._height = value
-		else:
-			print("Height cannot be negative.\n")
+    @classmethod
+    def creat(cls):
+        name = "unknown plant"
+        age = 0
+        height = 0
+        return cls(name, height, age)
 
-	@property
-	def proper_age(self):
-		return self._age
-	@proper_age.setter
-	def	proper_age(self, value: int):
-		if value >= 0:
-			self._age = value
-		else:
-			print("Age cannot be negative.\n")
+    def statistics(self):
+        print(f"\tTotal show calls: {self.count_show}")
+        print(f"\tTotal grow calls: {self.count_grow}")
+        print(f"\tTotal aged calls: {self.count_aged}")
+        print(f"\tTotal special calls: {self.count_special}")
 
-	"""Father method to be overridden by subclasses"""
-	def	show(self):
-		self.count_show += 1
-		print(f"{self.name.capitalize()}, {self._height:.2f}cm, {self._age} days old.")
+        """ === Getters and Setters for height and age with validation === """
+    @property
+    def proper_height(self):
+        return self._height
 
-	def	grow(self, days: int):
-		self.count_grow += 1
-		grow_fact = (self._height / self._age) if self._age > 0 else 0
-		const = grow_fact * (1 + (30 / (self._age + days)))
-		self._height += int(const)
-		self._age += days
+    @proper_height.setter
+    def proper_height(self, value: int):
+        if value > 0:
+            self._height = value
+        else:
+            print("Height cannot be negative.\n")
 
-	def	aged(self, days):
-		self.count_aged += 1
-		self.grow(days)
+    @property
+    def proper_age(self):
+        return self._age
 
-	@staticmethod
-	def	days2years(age: int):
-		if age >= 365:
-			years = age / 365
-			days = age % 365
-			return f"Total: {int(years)} years and {days} days"
-		else: 
-			return f"Total: {age} days < 1 year"		
+    @proper_age.setter
+    def proper_age(self, value: int):
+        if value >= 0:
+            self._age = value
+        else:
+            print("Age cannot be negative.\n")
 
-	""" === Flower sub-class === """
-class	Flowers(Plants):
-	def __init__(self, name:str, heigh:int, age:int, color:str, blooming:bool = False):
-		super().__init__(name, heigh, age)
-		self.color = color
-		self.blooming = blooming
+    """Father method to be overridden by subclasses"""
 
-	def bloom(self):
-		self.count_special += 1
-		if not self.blooming:
-			self.blooming = True
-			print(f"{self.name.capitalize()} is blooming with {self.color} color!.")
-	def	show(self):
-		super().show()
-		print(f"Color: {self.color}, Blooming: {'Yes' if self.blooming else 'No'}.")
+    def show(self):
+        self.count_show += 1
+        print(f"{self.name.capitalize()}, {
+            self._height:.2f}cm, {self._age} days old.")
 
-		""" === childish class"""
-class	Seed(Flowers):
-	def __init__(self, name:str, heigh:int, age:int, color:str, qty:int = 0):
-		super().__init__(name, heigh, age, color, blooming=False)
-		self.name = self.name + " seed"
-		self.qty = qty
-	def	show(self):
-		super().show()
-		print(f"Seeds: {self.qty if self.blooming else '0'}.")
-	def	grow(self, days: int):
-		super().grow(days)
-		self.qty += int((1 * days) * (10 / (self._age + days)))
+    def grow(self, days: int):
+        self.count_grow += 1
+        grow_fact = (self._height / self._age) if self._age > 0 else 0
+        const = grow_fact * (1 + (30 / (self._age + days)))
+        self._height += int(const)
+        self._age += days
 
-	""" === Tree sub-class === """
-class	Trees(Plants):
-	def __init__(self, name:str, heigh:int, age:int, trunk_diameter:float):
-		super().__init__(name, heigh, age)
-		self.trunk_diameter = trunk_diameter
+    def aged(self, days):
+        self.count_aged += 1
+        self.grow(days)
 
-	def	produce_shade(self):
-		self.count_special += 1
-		shade = (self._height * 0.5) + (self.trunk_diameter * 0.3)
-		print(f"{self.name.capitalize()} produces {shade:.2f} square centimeters of shade.\n")
-	def	show(self):
-		super().show()
-		print(f"Trunk Diameter: {self.trunk_diameter:.2f}cm")
-	def	grow(self, days: int):
-		super().grow(days)
-		self.trunk_diameter += (days * 0.1) * (self._height / self._age)
-	
-	""" === Vegetable sub-class === """
-class	Vegetables(Plants):
-	def __init__(self, name:str, heigh:int, age:int, harvest_season:str, nutritional_value:float = 0):
-		super().__init__(name, heigh, age)
-		self.harvest_season = harvest_season
-		self.nutritional_value = nutritional_value
-	def	show(self):
-		super().show()
-		print(f"Harvest Season: {self.harvest_season}, Nutritional Value: {self.nutritional_value:.2f}.\n")
-	def	grow(self, days: int):
-		super().grow(days)
-		self.nutritional_value += (1 * days) * (40 / (self._age + days))
+    @staticmethod
+    def days2years(age: int):
+        if age >= 365:
+            years = age / 365
+            days = age % 365
+            return f"Total: {int(years)} years and {days} days"
+        else:
+            return f"Total: {age} days < 1 year"
+
+    """ === Flower sub-class === """
 
 
-def	garden_analytics():
-	i:int = 150
-	sunflower = Flowers("sunflower", 30, 10, "yellow")
-	seed = Seed("sunflower", 1, 1, "yellow", qty=100)
-	oak = Trees("oak", 200, 100, 50)
-	tomato = Vegetables("tomato", 50, 20, "summer")
-	plant = Plants.creat()
-	list_plants = [sunflower, seed, oak, tomato]
-	print("=== Initial State ===\n")
-	for p in list_plants:
-		p.show()
-	plant.show()
-	print(f"\n=== After Growing for {i} days ===\n")
-	for p in list_plants:
-		p.aged(i)
-		p.show()
-	print("\n=== Special Actions ===\n")
-	sunflower.bloom()
-	oak.produce_shade()
-	print("\n=== Final Statistics ===\n")
-	for p in list_plants:
-		print(f"{p.name.capitalize()} statistics:")
-		p.statistics()
-	print(f"{plant.name.capitalize()} statistics:")
-	plant.statistics()
+class Flowers(Plants):
+    def __init__(
+            self,
+            name: str,
+            heigh: int,
+            age: int,
+            color: str,
+            blooming: bool = False):
+        super().__init__(name, heigh, age)
+        self.color = color
+        self.blooming = blooming
+
+    def bloom(self):
+        self.count_special += 1
+        if not self.blooming:
+            self.blooming = True
+            print(f"{self.name.capitalize()
+                     } is blooming with {self.color} color!.")
+
+    def show(self):
+        super().show()
+        print(
+            f"Color: {
+                self.color}, Blooming: {
+                'Yes' if self.blooming else 'No'}.")
+
+        """ === childish class"""
+
+
+class Seed(Flowers):
+    def __init__(
+            self,
+            name: str,
+            heigh: int,
+            age: int,
+            color: str,
+            qty: int = 0):
+        super().__init__(name, heigh, age, color, blooming=False)
+        self.name = self.name + " seed"
+        self.qty = qty
+
+    def show(self):
+        super().show()
+        print(f"Seeds: {self.qty if self.blooming else '0'}.")
+
+    def grow(self, days: int):
+        super().grow(days)
+        self.qty += int((1 * days) * (10 / (self._age + days)))
+
+    """ === Tree sub-class === """
+
+
+class Trees(Plants):
+    def __init__(self, name: str, heigh: int, age: int, trunk_diameter: float):
+        super().__init__(name, heigh, age)
+        self.trunk_diameter = trunk_diameter
+
+    def produce_shade(self):
+        self.count_special += 1
+        shade = (self._height * 0.5) + (self.trunk_diameter * 0.3)
+        print(
+            f"{self.name.capitalize()
+               } produces {shade:.2f} square centimeters of shade.\n")
+
+    def show(self):
+        super().show()
+        print(f"Trunk Diameter: {self.trunk_diameter:.2f}cm")
+
+    def grow(self, days: int):
+        super().grow(days)
+        self.trunk_diameter += (days * 0.1) * (self._height / self._age)
+
+    """ === Vegetable sub-class === """
+
+
+class Vegetables(Plants):
+    def __init__(
+            self,
+            name: str,
+            heigh: int,
+            age: int,
+            harvest_season: str,
+            nutritional_value: float = 0):
+        super().__init__(name, heigh, age)
+        self.harvest_season = harvest_season
+        self.nutritional_value = nutritional_value
+
+    def show(self):
+        super().show()
+        print(
+            f"Harvest Season: {
+                self.harvest_season}, Nutritional Value: {
+                self.nutritional_value:.2f}.\n")
+
+    def grow(self, days: int):
+        super().grow(days)
+        self.nutritional_value += (1 * days) * (40 / (self._age + days))
+
+
+def garden_analytics():
+    i: int = 150
+    sunflower = Flowers("sunflower", 30, 10, "yellow")
+    seed = Seed("sunflower", 1, 1, "yellow", qty=100)
+    oak = Trees("oak", 200, 100, 50)
+    tomato = Vegetables("tomato", 50, 20, "summer")
+    plant = Plants.creat()
+    list_plants = [sunflower, seed, oak, tomato]
+    print("=== Initial State ===\n")
+    for p in list_plants:
+        p.show()
+    plant.show()
+    print(f"\n=== After Growing for {i} days ===\n")
+    for p in list_plants:
+        p.aged(i)
+        p.show()
+    print("\n=== Special Actions ===\n")
+    sunflower.bloom()
+    oak.produce_shade()
+    print("\n=== Final Statistics ===\n")
+    for p in list_plants:
+        print(f"{p.name.capitalize()} statistics:")
+        p.statistics()
+    print(f"{plant.name.capitalize()} statistics:")
+    plant.statistics()
+
 
 if __name__ == "__main__":
-	garden_analytics()
+    garden_analytics()
